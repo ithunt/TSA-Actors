e46f532efab42ab3834527dec078c773d55f8bae
ian <ithunt0@gmail.com>
2012-02-08 17:19:41 -0500

    Security station implemented. Repo added to pom

--- src/main/java/SecurityStation.java
+++ src/main/java/SecurityStation.java
@@ -1,6 +1,9 @@
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author ian hunt
  * @date 2/8/12
@@ -17,9 +20,27 @@ public class SecurityStation extends UntypedActor {
     
     final int index;
     final ActorRef jail;
+    final Map<Passenger, Report> reports = new HashMap<Passenger, Report>();
+    boolean keepRunning = true;
 
     public SecurityStation(int index, ActorRef jail) {
         this.index = index;
         this.jail = jail;
+    }
+
+    public void onReceive(final Object message) {
+        if(message instanceof Report) {
+            final Report r = (Report)message;
+            if(reports.containsKey(r.p)) {
+                System.out.println(r.p + " leaves the Security Area");
+                reports.remove(r.p);
+            } else {
+                reports.put(r.p, r);
+            }
+        } else if (message instanceof Close) {
+            keepRunning = false;
+        }
+
+        if(!keepRunning && reports.isEmpty()) getContext().stop();
     }
 }
