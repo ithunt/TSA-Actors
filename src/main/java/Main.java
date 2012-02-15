737a92801f77ee85f94dbb08a4babc02f6c63dfa
Zach M <ztm4589@rit.edu>
2012-02-15 16:55:12 -0500

    initinalization logic in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,74 @@
  */
 public class Main {
     
-    public static void main(final String[] args) {
-
+    public static void main(final String[] args) 
+	{
+		List<Queue> queues = new List<Queue>(); 
+		
+		//create the Jail
+		theJail = actorOf(
+			new UntypedActorFactory()
+			{
+				public UntypedActor create()
+				{
+					return new Jail(3);
+				}
+			}
+		);
+		
+		for(int i = 0; i < 3; i++)
+		{
+			aSecurityStation = actorOf(
+				new UntypedActorFactory()
+				{
+					public UntypedActor create()
+					{
+						return new SecurityStation(i, theJail);
+					}
+				}
+			);
+			aBaggageScanner = actorOf(
+				new UntypedActorFactory()
+				{
+					public UntypedActor create()
+					{
+						return new BaggageScanner(i, aSecurityStation);
+					}
+				}
+			);
+			aBodyScanner = actorOf(
+				new UntypedActorFactory()
+				{
+					public UntypedActor create()
+					{
+						return new BodyScanner(i, aSecurityStation, /*ummm */);
+					}
+				}
+			);
+			aQueue = actorOf(
+				new UntypedActorFactory()
+				{
+					public UntypedActor create()
+					{
+						return new Queue(i, aBaggageScanner, aBodyScanner);
+					}
+				}
+			);
+			queues.add(aQueue);
+		}
+		
+		theDocumentChecker = actorOf(
+				new UntypedActorFactory()
+				{
+					public UntypedActor create()
+					{
+						return new DocumentScanner(queues);
+					}
+				}
+			);
+		
+		
+		
+		
     }
 }
